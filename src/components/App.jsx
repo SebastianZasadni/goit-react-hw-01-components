c087c135fcdee6510644b81bf3c5841d07ff731e
SebastianZasadni <emblelele@gmail.com>
2023-05-10 21:51:18 +0200

    check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,13 @@ import transactions from '../utils/transactions.json';
 
 export const App = () => (
   <>
-    <Profile
+    {/* <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
-    />
+    /> */}
     <Statistics title="Upload stats" stats={data} />
 
     <FriendList friends={friends} />
