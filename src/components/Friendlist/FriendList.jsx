645d82db215c0a033f09264e23150f125836a445
SebastianZasadni <112890476+SebastianZasadni@users.noreply.github.com>
2023-05-11 10:21:17 +0200

    fix
    change names

---
+++ src/components/Friendlist/FriendList.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+const checkStatus = isOnline => {
+  switch (isOnline) {
+    case true:
+      return 'green';
+    case false:
+      return 'red';
+    default:
+      return null;
+  }
+};
+
+export const FriendList = ({ friends }) => {
+  return (
+    <section className={css.friends}>
+      <ul className={css.friendlist}>
+        {friends.map(f => (
+          <li key={f.id} className={css.friendlistitem}>
+            <p
+              className={css.status}
+              style={{ backgroundColor: checkStatus(f.isOnline) }}
+            >
+              &nbsp;
+            </p>
+            <img className={css.avatar} src={f.avatar} alt="User avatar" />
+            <p className={css.name}>{f.name}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
